5c262d9b256da37e8e3a55afc600ad5da1ae9d40
Dmytro Kozin <dimakoua@gmail.com>
2024-08-04 01:24:08 +0300

    improve connection tests

--- lib/connection_test.exs
+++ lib/connection_test.exs
@@ -1,5 +1,5 @@
 defmodule HareMq.ConnectionTest do
-  use ExUnit.Case
+  use HareMq.TestCase
   alias HareMq.Connection
 
   setup do
@@ -8,11 +8,7 @@ defmodule HareMq.ConnectionTest do
 
   test "successfully connects and gets connection" do
     {:ok, _pid} = Connection.start_link(nil)
-
-    # Give some time to establish the connection
-    Process.sleep(100)
-
-    assert {:ok, %AMQP.Connection{}} = Connection.get_connection()
+    assert wait_until(fn -> {:ok, %AMQP.Connection{}} = Connection.get_connection() end)
   end
 
   test "fails to get connection when not connected" do
@@ -22,25 +18,19 @@ defmodule HareMq.ConnectionTest do
   test "successfully closes the connection" do
     {:ok, _pid} = Connection.start_link(nil)
 
-    # Give some time to establish the connection
-    Process.sleep(100)
-
-    assert {:ok, %AMQP.Connection{}} = Connection.get_connection()
+    assert wait_until(fn -> {:ok, %AMQP.Connection{}} = Connection.get_connection() end)
 
     # Close the connection
-    assert {:ok, %AMQP.Connection{}} = Connection.close_connection()
-
-    # Wait a bit to ensure the connection has time to close and the state to update
-    Process.sleep(100)
+    assert wait_until(fn -> {:ok, %AMQP.Connection{}} = Connection.close_connection() end)
 
     # Verify that the connection is indeed closed
-    assert {:error, :not_connected} = Connection.get_connection()
+    assert wait_until(fn -> {:error, :not_connected} = Connection.get_connection() end)
   end
 
   test "fails to close connection when not connected" do
     {:ok, _pid} = Connection.start_link(nil)
 
-    assert {:ok, %AMQP.Connection{}} = Connection.close_connection()
-    assert {:error, :not_connected} = Connection.close_connection()
+    assert wait_until(fn -> {:ok, %AMQP.Connection{}} = Connection.close_connection() end)
+    assert wait_until(fn -> {:error, :not_connected} = Connection.close_connection() end)
   end
 end
