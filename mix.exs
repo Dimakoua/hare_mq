d5f7c9000a4c259949288135c5006443e97a2667
Dmytro Kozin <dimakoua@gmail.com>
2026-05-22 01:26:46 +0000

    chore: update Elixir version requirement to ~> 1.14 and remove outdated OTP versions from CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HareMq.MixProject do
     [
       app: :hare_mq,
       version: "1.5.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
